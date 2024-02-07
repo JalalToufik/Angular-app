7ba4e2f61b1c5bebbbfec88cc692c00e1b1fdb6e
JalalToufik <112856590+JalalToufik@users.noreply.github.com>
2024-02-07 11:20:20 +0100

    Componenten aangemaakt
    Hier heb ik componenten aangemaakt en de data ingeladen.

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterLink, RouterOutlet, HomeComponent],
+  template: `
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'homes';
 }
