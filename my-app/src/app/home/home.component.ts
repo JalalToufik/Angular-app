020ef3712deaf34dc2d02a0beb03d89e051d9d0a
JalalToufik <112856590+JalalToufik@users.noreply.github.com>
2024-02-12 11:56:27 +0100

    Signals

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -47,7 +47,64 @@ export class HomeComponent {
     );
   }
 
-
 }
+
+
+// @Component({
+//   selector: 'app-home',
+//   standalone: true,
+//   imports: [CommonModule, HousingLocationComponent],
+//   template: `
+//     <section>
+//       <form (submit)="$event.preventDefault(); filterResults(filter.value); query.set(filter.value)">
+//         <input type="text" placeholder="Filter by city" #filter />
+//         <button class="primary" type="submit">Search</button>
+//       </form>
+//     </section>
+//     <section class="results">
+//       @for (housingLocation of filteredLocationList; track housingLocation.id){
+//         <app-housing-location [housingLocation]="housingLocation">
+//         </app-housing-location>
+//       }
+//     </section>
+//   `,
+//   styleUrl: './home.component.css'  
+// })
+
+// export class HomeComponent {
+//   readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+
+//   housingLocationList: WritableSignal<HousingLocation[]> = signal([]);
+//   test = computed(() => this.housingLocationList().length);
+//   housingService: HousingService = inject(HousingService);
+//   filteredLocationList: HousingLocation[] = []; 
+//   query = signal('');
+
+//   constructor() {
+//     this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+//       this.housingLocationList.set(housingLocationList);
+//       this.filteredLocationList = housingLocationList;
+//     });
+
+//   }
+
+//   filterResults(text: string) {
+//     if (!text) this.filteredLocationList = this.housingLocationList;
+
+//     this.filteredLocationList = this.housingLocationList.filter(
+//       housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+//     );
+//   }
+
+//   async httpresponse() {
+
+//   }
+
+
+// }
+
+
+
+
 
 
